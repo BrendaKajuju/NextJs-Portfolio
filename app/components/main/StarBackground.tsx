0bc6d71121321ccd1684a793fe3ff2d10d961d0f
Brenda <brendakajuju777@gmail.com>
2023-11-30 16:10:09 +0300

    Formatting Changes

--- app/components/main/StarBackground.tsx
+++ app/components/main/StarBackground.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-import React, { useState, useRef, Suspense } from "react";
+import React, 
+        {useState, 
+        useRef, 
+        Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 // @ts-ignore
